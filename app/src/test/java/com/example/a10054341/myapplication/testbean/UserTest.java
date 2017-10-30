3017bbb776a9d215c7795650beb0a06e17381c76
tsgemalt <tsursiean.gemalto@gmail.com>
2017-10-30 15:10:37 +0800

    -jenkins gradle 3.0 settings

--- app/src/test/java/com/example/a10054341/myapplication/testbean/UserTest.java
+++ app/src/test/java/com/example/a10054341/myapplication/testbean/UserTest.java
@@ -2,12 +2,17 @@ package com.example.a10054341.myapplication.testbean;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 /**
  * Created by 10054341 on 30/10/2017.
  */
 
 
+@Config(sdk = 18, manifest = Config.NONE)
+@RunWith(RobolectricTestRunner.class)
 public class UserTest {
     @Test
     public void UserNameTest() {
@@ -17,4 +22,9 @@ public class UserTest {
 
         Assert.assertEquals("foo", u.getName());
     }
+
+//    @Test
+//    public void failTest() {
+//        Assert.assertFalse(true);
+//    }
 }
